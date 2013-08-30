5ffbff1dbe212c77fe38e356180e4c22dd2ce385
Manuel Franco <mafraba@gmail.com>
2013-08-31 01:41:30 +0200

    Blog app. Setting root property.

---
+++ lib/blog.rb
@@ -0,0 +1,11 @@
+require 'sinatra/base'
+
+class Blog < Sinatra::Base
+  # File.expand_path generates an absolute path.
+  # It also takes a path as second argument. The
+  # generated path is treated as being relative
+  # to that path.
+  set :root, File.expand_path('../../', __FILE__)
+
+  puts "#{__FILE__} -> #{settings.root}"
+end
