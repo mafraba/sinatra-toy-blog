c8bb4f6e96f4cab224e710f3071b23ff014bd7be
Manuel Franco <mafraba@gmail.com>
2013-08-31 02:14:16 +0200

    Post view. Rendering posts.

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,11 +1,26 @@
 require 'sinatra/base'
+require 'ostruct'
+require 'time'
+require 'YAML'
 
 class Blog < Sinatra::Base
-  # File.expand_path generates an absolute path.
-  # It also takes a path as second argument. The
-  # generated path is treated as being relative
-  # to that path.
   set :root, File.expand_path('../../', __FILE__)
-
-  puts "#{__FILE__} -> #{settings.root}"
+
+  # loop through all the article files
+  Dir.glob "#{root}/articles/*.md" do |file|
+    # parse meta data and content from file
+    meta, content = File.read(file).split("\n\n", 2)
+    # generate a metadata object
+    article = OpenStruct.new YAML.load(meta)
+    # convert the date to a time object
+    article.date = Time.parse article.date.to_s
+    # add the content
+    article.content = content
+    # generate a slug for the url
+    article.slug = File.basename(file, '.md')
+    # set up the route
+    get "/#{article.slug}" do
+      erb :post, :locals => { :article => article }
+    end
+  end
 end
